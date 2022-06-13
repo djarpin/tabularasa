e85d1228608e36055057c7c679bc75ade036375e
David Arpin <arpin.david@gmail.com>
2022-06-12 23:02:52 -0700

    ADDED: simultaneous quantile regressor class

--- tabularasa/OrthonormalCertificates.py
+++ tabularasa/OrthonormalCertificates.py
@@ -9,10 +9,12 @@ class OrthonormalCertificates:
                  dim_certificates=64,
                  epochs=500,
                  batch_size=128,
+                 alpha=1
                  shuffle=True):
         self.dim_certificates = dim_certificates
         self.epochs = epochs
         self.batch_size = batch_size
+        self.alpha = alpha
         self.shuffle = shuffle
         # Add final loss
 
@@ -26,13 +28,15 @@ class OrthonormalCertificates:
         for epoch in range(self.epochs):
             for xi in loader:
                 opt.zero_grad()
+                # Maybe have this support other loss functions than MSE
                 error = self.certificates(xi[0]).pow(2).mean()
                 penalty = (self.certificates.weight @ self.certificates.weight.t() -
                            torch.eye(self.dim_certificates)).pow(2).mean()
-                (error + penalty).backward()
+                (error + self.alpha * penalty).backward()
                 opt.step()
         # Set and report on final loss
         # Do I need data standardization?  Optionally?  scikit moving away from this
+        # Use a validation dataset to set the threshold?  This would allow me to create a score function
 
     def transform(self, X):
         # Should probably do some type checking here
